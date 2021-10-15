0afdf304ec024f0d64b425716d42f35ed20dc46d
Khélian Larvet <42818039+Abralixis@users.noreply.github.com>
2021-10-15 15:00:20 +0200

    update fire

---
+++ fire.cpp
@@ -0,0 +1,41 @@
+#include "fire.h"
+#include "batiment.h"
+#include "minion.h"
+#include <cstddef>
+#include <stdlib.h>
+#include <time.h>
+#define PROPAGATION_CHANCE 3
+Fire::Fire(int x, int y){
+	this->pos_x = x;
+	this->pos_y = y;
+}
+void Fire::action(Batiment * batiment){
+	if (rand() % PROPAGATION_CHANCE == 0) {
+		this->propagate(batiment->getCase(this->getX() - 1, this->getY()));
+	}
+	if (rand() % PROPAGATION_CHANCE == 0) {
+		this->propagate(batiment->getCase(this->getX() + 1, this->getY()));
+	}
+	if (rand() % PROPAGATION_CHANCE == 0) {
+		this->propagate(batiment->getCase(this->getX(), this->getY() - 1));
+	}
+	if (rand() % PROPAGATION_CHANCE == 0) {
+		this->propagate(batiment->getCase(this->getX(), this->getY() + 1));
+	}
+}
+
+void Fire::propagate(Case * emplacement){
+	if(emplacement != NULL){
+		bool fire = false;
+		switch(emplacement->getState()){
+			case StateEnum::minion : 
+				((Minion*)emplacement->getAgent())->popcorn();
+				emplacement->setState(StateEnum::popCorn);
+				break;
+			case StateEnum::empty :
+				emplacement->setState(StateEnum::flame);
+				emplacement->setAgent(new Fire(emplacement->getX(), emplacement->getY()));
+				break;
+		}
+	}
+}
